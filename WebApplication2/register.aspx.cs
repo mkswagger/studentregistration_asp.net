d795aee3c6e770ed1c78ace11e9d61f828795f28
mkswagger <mathangykrishna16@gmail.com>
2023-06-15 01:14:42 +0530

    .cs part of RegisterBtn 🧨

--- WebApplication2/register.aspx.cs
+++ WebApplication2/register.aspx.cs
@@ -17,8 +17,18 @@ namespace WebApplication2
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-            lblMessage.Text = "Invalid Username or password";
-            lblMessage.ForeColor = Color.Red;
+           string name=txtName.Text.ToString();
+           string mobile=txtMobile.Text.ToString();
+           string email=txtEmail.Text.ToString();
+           string course=ddlCourse.SelectedItem.ToString(); 
+           string date=Calender.Text.ToString();
+           DateTime dob= DateTime.Parse(date);
+            if (ImgUpload.HasFiles)
+            {
+                string filename=ImgUpload.FileName;
+                ImgUpload.SaveAs(Server.MapPath("images//" + ImgUpload.FileName));
+            }
+           
         }
 
         protected void btnReset_Click(object sender, EventArgs e)
