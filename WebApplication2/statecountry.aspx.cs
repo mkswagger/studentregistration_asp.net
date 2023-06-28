17011a30e20cb41d237aa3f7819edcc3a59fcddc
mkswagger <mathangykrishna16@gmail.com>
2023-06-28 10:34:31 +0530

    added submit and cancel button ,search option 🧨

--- WebApplication2/statecountry.aspx.cs
+++ WebApplication2/statecountry.aspx.cs
@@ -191,44 +191,25 @@ namespace WebApplication2
         }
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            string countryName = txtNewCountry.Text;
-            string stateName = txtNewState.Text;
+            string countryId = ddlCountry.SelectedValue;
+            string stateId = ddlState.SelectedValue;
 
             using (SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["conString"].ConnectionString))
             {
-                con.Open();
+                string query = "INSERT INTO state_master (CountryID, StateName) VALUES (@CountryID, @StateName)";
 
-                // Insert the new country into the country_master table
-                string countryInsertQuery = "INSERT INTO country_master (CountryName) VALUES (@CountryName)";
-                using (SqlCommand countryInsertCmd = new SqlCommand(countryInsertQuery, con))
+                using (SqlCommand cmd = new SqlCommand(query, con))
                 {
-                    countryInsertCmd.Parameters.AddWithValue("@CountryName", countryName);
-                    countryInsertCmd.ExecuteNonQuery();
-                }
+                    cmd.Parameters.AddWithValue("@CountryID", countryId);
+                    cmd.Parameters.AddWithValue("@StateName", ddlState.SelectedItem.Text);
 
-                // Retrieve the newly inserted country ID
-                string countryIdQuery = "SELECT SCOPE_IDENTITY()";
-                using (SqlCommand countryIdCmd = new SqlCommand(countryIdQuery, con))
-                {
-                    int countryId = Convert.ToInt32(countryIdCmd.ExecuteScalar());
-
-                    // Insert the new state into the state_master table
-                    string stateInsertQuery = "INSERT INTO state_master (StateName, CountryID) VALUES (@StateName, @CountryID)";
-                    using (SqlCommand stateInsertCmd = new SqlCommand(stateInsertQuery, con))
-                    {
-                        stateInsertCmd.Parameters.AddWithValue("@StateName", stateName);
-                        stateInsertCmd.Parameters.AddWithValue("@CountryID", countryId);
-                        stateInsertCmd.ExecuteNonQuery();
-                    }
+                    con.Open();
+                    cmd.ExecuteNonQuery();
                 }
             }
 
-            // Reset the form and rebind the data
-            pnlAdd.Visible = false;
             pnlShow.Visible = true;
-            //pnlSearch.Visible = true;
-            //txtNewCountry.Text = string.Empty;
-            //txtNewState.Text = string.Empty;
+            pnlAdd.Visible = false;
 
             BindStateCountry();
         }
